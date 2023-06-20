6f9625f838a2931d84fc691638f40891ade62704
Akshata Chawan <akshtachawan@gmail.com>
2023-06-20 23:34:28 +0530

    second commit

---
+++ cities.js
@@ -0,0 +1,362 @@
+export default [
+  {
+    country: "IN",
+    name: "Mumbai",
+  },
+  {
+    country: "IN",
+    name: "Delhi",
+  },
+  {
+    country: "IN",
+    name: "Bengaluru",
+  },
+  {
+    country: "IN",
+    name: "Kolkata",
+  },
+  {
+    country: "IN",
+    name: "Chennai",
+  },
+  {
+    country: "IN",
+    name: "Ahemedabad",
+  },
+  {
+    country: "IN",
+    name: "Hyderabad",
+  },
+  {
+    country: "IN",
+    name: "Pune",
+  },
+  {
+    country: "IN",
+    name: "Kanpur",
+  },
+  {
+    country: "IN",
+    name: "Jaipur",
+  },
+  {
+    country: "IN",
+    name: "Navi Mumbai",
+  },
+  {
+    country: "IN",
+    name: "Lucknow",
+  },
+  {
+    country: "IN",
+    name: "Nagpur",
+  },
+  {
+    country: "IN",
+    name: "Indore",
+  },
+  {
+    country: "IN",
+    name: "Patna",
+  },
+  {
+    country: "IN",
+    name: "Bhopal",
+  },
+  {
+    country: "IN",
+    name: "Ludhiana",
+  },
+  {
+    country: "IN",
+    name: "Tirunelveli",
+  },
+  {
+    country: "IN",
+    name: "Agra",
+  },
+  {
+    country: "IN",
+    name: "Vadodara",
+  },
+  {
+    country: "IN",
+    name: "Gorakhpur",
+  },
+  {
+    country: "IN",
+    name: "Nashik",
+  },
+  {
+    country: "IN",
+    name: "Pimpri",
+  },
+  {
+    country: "IN",
+    name: "Kalyan",
+  },
+  {
+    country: "IN",
+    name: "Thane",
+  },
+  {
+    country: "IN",
+    name: "Meerut",
+  },
+  {
+    country: "IN",
+    name: "Faridabad",
+  },
+  {
+    country: "IN",
+    name: "Ghaziabad",
+  },
+  {
+    country: "IN",
+    name: "Dombivali",
+  },
+  {
+    country: "IN",
+    name: "Rajkot",
+  },
+  {
+    country: "IN",
+    name: "Varanasi",
+  },
+  {
+    country: "IN",
+    name: "Amritsar",
+  },
+  {
+    country: "IN",
+    name: "Prayagraj",
+  },
+  {
+    country: "IN",
+    name: "Visakhapatnam",
+  },
+  {
+    country: "IN",
+    name: "Jabalpur",
+  },
+  {
+    country: "IN",
+    name: "Howrah",
+  },
+  {
+    country: "IN",
+    name: "Aurangabad",
+  },
+  {
+    country: "IN",
+    name: "Solapur",
+  },
+  {
+    country: "IN",
+    name: "Srinagar",
+  },
+  {
+    country: "IN",
+    name: "Chandigarh",
+  },
+  {
+    country: "IN",
+    name: "Coimbatore",
+  },
+  {
+    country: "IN",
+    name: "Jodhpur",
+  },
+  {
+    country: "IN",
+    name: "Madurai",
+  },
+  {
+    country: "IN",
+    name: "Guwahati",
+  },
+  {
+    country: "IN",
+    name: "Mysore",
+  },
+  {
+    country: "IN",
+    name: "Ranchi",
+  },
+  {
+    country: "IN",
+    name: "Hubli",
+  },
+  {
+    country: "IN",
+    name: "Jalandhar",
+  },
+  {
+    country: "IN",
+    name: "Tiruchirappali",
+  },
+  {
+    country: "IN",
+    name: "Kota",
+  },
+  {
+    country: "IN",
+    name: "Bhubaneshwar",
+  },
+  {
+    country: "IN",
+    name: "Aligarh",
+  },
+  {
+    country: "IN",
+    name: "Bhiwandi",
+  },
+  {
+    country: "IN",
+    name: "Raipur",
+  },
+  {
+    country: "IN",
+    name: "Gorakhpur",
+  },
+  {
+    country: "IN",
+    name: "Jamshedpur",
+  },
+  {
+    country: "IN",
+    name: "Borivali",
+  },
+  {
+    country: "IN",
+    name: "Kochi",
+  },
+  {
+    country: "IN",
+    name: "Amravati",
+  },
+  {
+    country: "IN",
+    name: "Sangli",
+  },
+  {
+    country: "IN",
+    name: "Bikaner",
+  },
+  {
+    country: "IN",
+    name: "Bikaner",
+  },
+  {
+    country: "IN",
+    name: "Warangal",
+  },
+  {
+    country: "IN",
+    name: "Nanded",
+  },
+  {
+    country: "IN",
+    name: "Dehradun",
+  },
+  {
+    country: "IN",
+    name: "Durgapur",
+  },
+  {
+    country: "IN",
+    name: "Ajmer",
+  },
+  {
+    country: "IN",
+    name: "Ulhasnagar",
+  },
+  {
+    country: "IN",
+    name: "Kolhapur",
+  },
+  {
+    country: "IN",
+    name: "Siliguri",
+  },
+  {
+    country: "IN",
+    name: "Jamnagar",
+  },
+  {
+    country: "IN",
+    name: "Jammu",
+  },
+  {
+    country: "IN",
+    name: "Ujjain",
+  },
+  {
+    country: "IN",
+    name: "Kozhikode",
+  },
+  {
+    country: "IN",
+    name: "Malegaon",
+  },
+  {
+    country: "IN",
+    name: "Jalgaon",
+  },
+  {
+    country: "IN",
+    name: "Akola",
+  },
+  {
+    country: "IN",
+    name: "Ratnagiri",
+  },
+  {
+    country: "IN",
+    name: "Latur",
+  },
+  {
+    country: "IN",
+    name: "Germany",
+  },
+  {
+    country: "IN",
+    name: "London",
+  },
+  {
+    country: "IN",
+    name: "Australia",
+  },
+  {
+    country: "IN",
+    name: "New Zealand",
+  },
+  {
+    country: "IN",
+    name: "Singapore",
+  },
+  {
+    country: "IN",
+    name: "Russia",
+  },
+  {
+    country: "IN",
+    name: "Brazil",
+  },
+  {
+    country: "IN",
+    name: "Canada",
+  },
+  {
+    country: "IN",
+    name: "Italy",
+  },
+  {
+    country: "IN",
+    name: "Japan",
+  },
+  {
+    country: "IN",
+    name: "France",
+  },
+];
